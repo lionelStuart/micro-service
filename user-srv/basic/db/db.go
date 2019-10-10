47ba3088afcc6964066d672d7f4293789ce90f2a
a_z <lisi_stuart@126.com>
2019-10-11 07:27:29 +0800

    complete conf

--- user-srv/basic/db/db.go
+++ user-srv/basic/db/db.go
@@ -1,1 +1,38 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-log/log"
+	"sync"
+	"user-srv/basic/config"
+)
+
+var (
+	inited  bool
+	mysqlDB *sql.DB
+	m       sync.RWMutex
+)
+
+func Init() {
+	m.Lock()
+	defer m.Unlock()
+
+	var err error
+
+	if inited {
+		err = fmt.Errorf("[Init] db inited already")
+		log.Logf(err.Error())
+		return
+	}
+
+	// init mysql if enabled
+	if config.GetMysqlConfig().GetEnabled() {
+		initMysql()
+	}
+	inited = true
+}
+
+func GetDB() *sql.DB {
+	return mysqlDB
+}
--- user-srv/basic/db/mysql.go
+++ user-srv/basic/db/mysql.go
@@ -1,1 +1,28 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+	"user-srv/basic/config"
+)
+
+func initMysql() {
+	var err error
+
+	// open mysql on conf url
+	mysqlDB, err = sql.Open("mysql", config.GetMysqlConfig().GetURL())
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	//max idle connections
+	mysqlDB.SetMaxIdleConns(config.GetMysqlConfig().GetMaxIdleConnection())
+
+	//max open connections
+	mysqlDB.SetMaxOpenConns(config.GetMysqlConfig().GetMaxOpenConnection())
+
+	if err = mysqlDB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
